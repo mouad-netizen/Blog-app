4b5e90f2fca9b5e79783c85b2766f0a017a8207b
= <=>
2024-01-31 23:42:28 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,59 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { CategoryNavbarComponent } from './layouts/category-navbar/category-navbar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsAndConditionsComponent } from './pages/terms-and-conditions/terms-and-conditions.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { SubscribionFormComponent } from './subscribion-form/subscribion-form.component';
+import { CommentFormComponent } from './comments/comment-form/comment-form.component';
+import { CommentListComponent } from './comments/comment-list/comment-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
+
+
+const routes : Routes = [
+  {path:'',component:HomeComponent},
+  {path:'category',component:SingleCategoryComponent},
+  {path:'post',component:SinglePostComponent},
+
+
+  {path:'about',component:AboutUsComponent},
+  {path:'terms-and-conditions',component:TermsAndConditionsComponent},
+  {path:'contact',component:ContactUsComponent},
+
+
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoryNavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    SingleCategoryComponent,
+    SinglePostComponent,
+    TermsAndConditionsComponent,
+    ContactUsComponent,
+    SubscribionFormComponent,
+    CommentFormComponent,
+    CommentListComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
+  exports:[RouterModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/subscribion-form/subscribion-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribion-form',
+  templateUrl: './subscribion-form.component.html',
+  styleUrls: ['./subscribion-form.component.css']
+})
+export class SubscribionFormComponent {
+
+}
